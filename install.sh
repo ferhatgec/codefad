ebf8bd48d07293b163778b9896b88c1a309b6b59
ferhatgec <ferhatgectao@gmail.com>
2020-10-01 23:51:55 +0300

    Add new icons.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Required Vala to C compiler, GTK3 library, GtkSourceView (devel) and GCC.
 valac --pkg gtk+-3.0 --pkg gtksourceview-4 Settings.vala CodeFad.vala -o /bin/codefad 
 
-cp resource/codefad.png /usr/share/pixmaps
+mkdir /usr/share/pixmaps/codefad/
+cp resource/*.png /usr/share/pixmaps/codefad/
 cp codefad.desktop /usr/share/applications/
 
